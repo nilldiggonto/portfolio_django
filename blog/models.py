a52ffd8f47e59480f2d693ca94694b5f0e7f0a88
unknown <nilldiggonto@gmail.com>
2019-02-25 04:16:20 +0600

    Work with job done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Blog(models.Model):
+    title       = models.CharField(max_length=250)
+    pub_date    = models.DateTimeField()
+    body        = models.TextField()
+    image       = models.ImageField(upload_to='images/blog/%y/%m/%d')
+
+    def __str__(self):
+        return self.title
+    
+
+
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+def home(request):
+    queryset = Job.objects.all()
+
+    context = {
+        'jobs': queryset,
+    }
+    return render(request,'jobs/home.html',context)
